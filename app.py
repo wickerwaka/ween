091a16e908f65f74b95c2afe39e7c7de728015d1
Martin Donlon <github-martin@donlons.com>
2016-10-21 05:01:29 +0000

    Added rpi gpio module

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+#! /usr/bin/env python
+
 import bottle
 from bottle import abort, Bottle, static_file
 
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
 relay_state = [ 0 for x in range(8) ]
-gpio_ports = [ 17, 27, 22, 5, 6, 13, 19, 26 ]
+gpio_ports = [ 26, 19, 13, 6, 5, 22, 27, 17 ]
+
+GPIO.setup( gpio_ports, GPIO.OUT, initial=GPIO.HIGH )
+
 
 app = Bottle()
 
@@ -36,13 +45,17 @@ def set_relay(id, action):
 		abort( 400, 'Out of range relay ID' )
 
 	if action == 'on':
+		GPIO.output( gpio_ports[id], GPIO.LOW )
 		relay_state[id] = 1
 	elif action == 'off':
+		GPIO.output( gpio_ports[id], GPIO.HIGH )
 		relay_state[id] = 0
 	elif action == 'toggle':
 		if old_state == 0:
+			GPIO.output( gpio_ports[id], GPIO.LOW )
 			relay_state[id] = 1
 		else:
+			GPIO.output( gpio_ports[id], GPIO.HIGH )
 			relay_state[id] = 0
 
 	return { 'state': relay_state[id], 'old_state': old_state  }
